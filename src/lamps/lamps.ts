c828346e1f9962f56bb2062fa3de945bbe9feb1a
DMatvienko <dmatvienko@griddynamics.com>
2019-11-11 06:42:08 +0400

    feat: adds get by id and get all

--- src/lamps/lamps.ts
+++ src/lamps/lamps.ts
@@ -5,6 +5,7 @@ import { Lamp } from './lamp';
 import {
   create,
   findById,
+  getAll,
   update,
   deleteLamp
 } from './lampsData';
@@ -39,6 +40,51 @@ router.post(
 
     const created = create(name, age);
     response.send(created);
+  }
+);
+
+const lampIdentificatorParamName = 'id';
+router.get(
+  `/:${lampIdentificatorParamName}`,
+  (request, response) => {
+    const idParamValue = request.params[lampIdentificatorParamName];
+
+    const id = Number(idParamValue);
+
+    if (isNaN(id)) {
+      response
+        .status(400)
+        .send('incorrect id');
+
+      throw {
+        id: idParamValue,
+        message: 'incorrect id'
+      };
+    }
+
+    const found = findById(id);
+
+    if (!found) {
+      response
+        .status(400)
+        .send('lamp with specified id was not found');
+
+      throw {
+        id,
+        message: 'there is no lamp with specified id'
+      };
+    }
+
+    response.send(found);
+  }
+);
+
+router.get(
+  `/`,
+  (_, response) => {
+    const lamps = getAll();
+
+    response.send(lamps);
   }
 );
 
